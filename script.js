f120f12828b957162b6a6c9104eb1a26836719ee
Celeste Loi <loiceleste94@gmail.com>
2024-03-17 19:19:14 +0100

    tidy up the code

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-//get the button
+//get the submit button
 let submitBtn = document.getElementById("submitBtn");
 //get the user input field
 let userGuess = document.getElementById("userGuess");
-// Add click event to the button
-submitBtn.addEventListener(
-  "click",
-  function () {
-    checkResult(userGuess.value);
-  },
-  false
-);
-//takes in a guess
+// Add click event to the submit button
+submitBtn.addEventListener("click", function () {
+  //call the checkResult function passing the user chosen number as parameter
+  checkResult(userGuess.value);
+});
+
+//Game logic function; takes the user guessed number as parameter
 function checkResult(userGuess) {
-  //generates a random number
+  //generates a random number between 1-20
   let num = Math.floor(Math.random() * 20) + 1;
   console.log(num);
-  //retrieve the hidden p to show user the results
+  //retrieve the hidden p in the DOM to show user the results
   let results = document.getElementById("results");
-  //returns a string with the guess and whether that guess is lower or higher than the random number
-  //e.g. 'you answered 9. The correct answer is higher'
+  //if the user guessed number is higher than the randomly generated number
   if (userGuess > num) {
+    // show the results p by removing the hidden class
     results.removeAttribute("class");
-    results.innerHTML = "You guess is too high!";
+    // outputs to the user that the guessed number is too high
+    results.innerHTML =
+      "You answered " + userGuess + ". The correct answer is lower!";
   } else if (userGuess < num) {
+    // show the results p by removing the hidden class
     results.removeAttribute("class");
-    results.innerHTML = "You guess is too low!";
+    // outputs to the user that the guessed number is too low
+    results.innerHTML =
+      "You answered " + userGuess + ". The correct answer is higher!";
   } else {
+    // show the results p by removing the hidden class
     results.removeAttribute("class");
-    results.innerHTML = "You guessed it right!";
+    // outputs to the user that he guessed right!
+    results.innerHTML =
+      "You answered " + userGuess + ". This is the correct answer!";
   }
 }
-//returns a string with the guess and whether that guess is lower or higher than the random number
-//e.g. 'you answered 9. The correct answer is higher'
-
-//If the guess matches the random number, then returns a string which confirms this.
-//e.g.'you answered 9. This is the correct answer'
-
-//ranges
 
 //input validation
 
