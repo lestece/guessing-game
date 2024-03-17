8c9a354ffe8f9e7a9e84e92f8dea4b69d80ae579
Celeste Loi <loiceleste94@gmail.com>
2024-03-17 20:00:59 +0100

    add attempts constraint

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+//generates a random number between 1-20
+let num = Math.floor(Math.random() * 20) + 1;
+console.log(num);
+//Initialize an attempt counter
+let attemptCounter = 1;
+//establish an attempts limit
+let maxAttempts = 3;
 //get the submit button
 let submitBtn = document.getElementById("submitBtn");
 //get the user input field
 let userGuess = document.getElementById("userGuess");
+//get the attempt number p
+let attempts = document.getElementById("attempt");
+attempts.innerHTML = "Attempt number:" + attemptCounter;
 
 //Number validation for the input field
 function numberValidation(n) {
@@ -23,30 +33,34 @@ submitBtn.addEventListener("click", function () {
 
 //Game logic function; takes the user guessed number as parameter
 function checkResult(userGuess) {
-  //generates a random number between 1-20
-  let num = Math.floor(Math.random() * 20) + 1;
-  console.log(num);
-  //retrieve the hidden p in the DOM to show user the results
-  let results = document.getElementById("results");
-  //if the user guessed number is higher than the randomly generated number
-  if (userGuess > num) {
-    // show the results p by removing the hidden class
-    results.removeAttribute("class");
-    // outputs to the user that the guessed number is too high
-    results.innerHTML =
-      "You answered " + userGuess + ". The correct answer is lower!";
-  } else if (userGuess < num) {
-    // show the results p by removing the hidden class
-    results.removeAttribute("class");
-    // outputs to the user that the guessed number is too low
-    results.innerHTML =
-      "You answered " + userGuess + ". The correct answer is higher!";
+  if (attemptCounter < maxAttempts) {
+    //retrieve the hidden p in the DOM to show user the results
+    let results = document.getElementById("results");
+    //if the user guessed number is higher than the randomly generated number
+    if (userGuess > num) {
+      // show the results p by removing the hidden class
+      results.removeAttribute("class");
+      // outputs to the user that the guessed number is too high
+      results.innerHTML =
+        "You answered " + userGuess + ". The correct answer is lower!";
+    } else if (userGuess < num) {
+      // show the results p by removing the hidden class
+      results.removeAttribute("class");
+      // outputs to the user that the guessed number is too low
+      results.innerHTML =
+        "You answered " + userGuess + ". The correct answer is higher!";
+    } else {
+      // show the results p by removing the hidden class
+      results.removeAttribute("class");
+      // outputs to the user that he guessed right!
+      results.innerHTML =
+        "You answered " + userGuess + ". This is the correct answer!";
+    }
+    //increment attempts by 1
+    attemptCounter++;
+    attempts.innerHTML = "Attempt number:" + attemptCounter;
   } else {
-    // show the results p by removing the hidden class
-    results.removeAttribute("class");
-    // outputs to the user that he guessed right!
-    results.innerHTML =
-      "You answered " + userGuess + ". This is the correct answer!";
+    results.innerHTML = "You lose!";
   }
 }
 
